013bff276b9cdd303da6b9acacfac89ca916d72b
rafael <rafael@rafael-VirtualBox>
2019-10-03 16:20:07 +0200

    Un poquito mas de desarrollo

--- src/main/java/com/rafa/service/SampleService.java
+++ src/main/java/com/rafa/service/SampleService.java
@@ -1,14 +1,26 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.rafa.service;
+
+import com.rafa.repository.SampleRepository;
+import org.springframework.stereotype.Service;
 
 /**
  *
  * @author rafael
  */
+
+@Service
 public class SampleService {
+
+    private SampleRepository sampleRepository;
+    
+    public SampleService (SampleRepository sampleRepository){
+        this.sampleRepository = sampleRepository;
+    }
+    
+    public String welcome(String username){
+        //sampleRepository.
+        return null;
+    }
+    
     
 }
